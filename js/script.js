353504188996aa2d605d4b731eb479a2b0a2833b
Mozzy9328 <74562419+Mozzy9328@users.noreply.github.com>
2021-11-16 18:00:28 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const active = document.querySelectorAll(".nav-link");
-const theCards = document.querySelectorAll(".tab-content");
+// Import just what we need
 
-function activation() {
-	for (let i = 0; i < active.length; i++) {
-		active[i];
-	}
-
-	activation.onclick = function () {
-		for (let i = 0; i < theCards.length; i++) {
-			theCards[i].style.display = "block";
-		}
-	};
-}
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+import "bootstrap/js/dist/tab";
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
